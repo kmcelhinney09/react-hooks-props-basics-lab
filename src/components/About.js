6db5da301fbb37389dfa6f3f9788dea31647e076
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-11-27 13:43:53 -0800

    commit tree c530b54b7c90e1f805dac259259b2248ca5601fc

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function renderBio(bio){
+  if (bio === undefined || bio.length < 1){
+    return null
+  }
+  else{
+    return <p>{bio}</p>
+  }
+}
+
+function About(props) {
+  const bio = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {renderBio(bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
